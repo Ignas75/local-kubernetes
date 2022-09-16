92cccbf8bbf6b1c65b4b6627dec5797aa9cb1924
unknown <ignasserepka75@gmail.com>
2022-09-16 14:51:53 +0100

    feat: added cluster creation and deletion scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+# getting kind cluster name from arguments
+if [ $? == 1 ]
+then 
+    cluster_name=$1
+else 
+    cluster_name="hobbies"
+fi
+
+# creating the kind cluster
+kind create cluster --name=${cluster_name}
+
+# storing the kind cluster name in a file for use in deletion
+mkdir temp
+cd temp 
+echo ${cluster_name} > cluster-name
